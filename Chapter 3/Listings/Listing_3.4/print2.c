fe4f890a7e4a4fdc7c00eeeae73e8b14863dca91
Cybernetyc <Necromounger1990@gmail.com>
2025-06-03 16:51:46 +0300

    Added Listing_3.5 (charcode.c + CMakeLists.txt)

--- Chapter 3/Listings/Listing_3.4/print2.c
+++ Chapter 3/Listings/Listing_3.4/print2.c
@@ -1,21 +1,18 @@
 /*
- * print2.c -- Additional properties of printf() function.
+* print2.cpp -- Additional properties of printf() function with graphics
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.graphics.directx.h>
+#include "raylib.h"
 
-int main(void)
+int main (void)
 {
-  const unsigned int un = 3000000000; ///< system with 32-bit int type
-  const short end = 200;              ///< and 16-bit short type
-  const long big = 65537;
-  const long long verybig = 12345678908642;
 
-  printf("un = %u, but no %d\n", un, un);
-  printf("end = %hd and %d\n", end, end);
-  printf("big = %ld, but not %hd\n",big, big);
-  printf("verybig = %lld, but not %ld\n", verybig, verybig);
 
-  return EXIT_SUCCESS;
+    const unsigned int un = 3000000000; ///< system with 32-bit int type
+    const short end = 200;              ///< and 16-bit short type
+    const long big = 65537;
+    const long long verybig = 12345678908642;
+
+    return EXIT_SUCCESS;
 }
