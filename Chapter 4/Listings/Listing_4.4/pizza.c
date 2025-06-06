8e57dfe60fd055a5fce59af8c27389c06df83b29
Cybernetyc <Necromounger1990@gmail.com>
2025-06-06 13:01:06 +0300

    Added Listing_4.4 (pizza.c + CMakeLists.txt)

---
+++ Chapter 4/Listings/Listing_4.4/pizza.c
@@ -0,0 +1,23 @@
+/**
+ * pizza.c -- This program is used some constants, certain in relationship to pizza
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define  PI (3.14159)
+
+int main(void)
+{
+  float radius = 0;
+
+  printf("Enter the radius of the pizza: ");
+  scanf("%f", &radius);
+
+  const float area = PI * radius * radius;
+  const float circum = 2.0f * PI * radius;
+
+  printf("The main parameters of your pizza is:\n");
+  printf("Area ---------------------------->|| %.2f\n", area);
+  printf("Length of the circle ------------>|| %.2f\n", circum);
+  return EXIT_SUCCESS;
+}
