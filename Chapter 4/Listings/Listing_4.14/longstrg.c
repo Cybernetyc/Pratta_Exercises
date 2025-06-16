c1a7425e1fdb286c4add70bd8b44c5f4166dae36
Cybernetyc <Necromounger1990@gmail.com>
2025-06-16 16:42:00 +0300

    Added Listing_4.14 (longstrg.c + CMakeLists.txt)

---
+++ Chapter 4/Listings/Listing_4.14/longstrg.c
@@ -0,0 +1,19 @@
+/*
+ * longstrg.c -- The output of long string
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  printf("Here is one way ");
+  printf("to withdraw a long line\n");
+
+  printf("Here is second way \
+to withdraw a long line.\n");
+
+  printf("And here is newest way "
+    "to withdraw a long line.\n");
+
+  return EXIT_SUCCESS;
+}
