6301ba4f5a3c2484aab36d886a24abec54e98bfc
Cybernetyc <Necromounger1990@gmail.com>
2025-06-21 08:48:12 +0300

    Added Listing_4.17 (skip_two.c + CMakeLists.txt)

---
+++ Chapter 4/Listings/Listing_4.17/skip_two.c
@@ -0,0 +1,16 @@
+/**
+ * skiptwo.c -- Misses first two integer numbers from input stream.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int вася, ваня, дима;
+
+    scanf("%d %d %d", &вася, &ваня, &дима);
+    printf("%9d | %9d | %9d\n", вася , ваня , дима);
+
+
+  return EXIT_SUCCESS;
+}
