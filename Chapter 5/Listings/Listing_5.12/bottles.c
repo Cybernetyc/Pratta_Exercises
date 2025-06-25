b5ff1c7992ec3515f3d44c6a2368b8b1e877d08d
Cybernetyc <Necromounger1990@gmail.com>
2025-06-25 16:04:13 +0300

    Added Listing_5.12 (bootles.c + CMakeLists.txt)

---
+++ Chapter 5/Listings/Listing_5.12/bottles.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX (100)
+
+int main(void)
+{
+  int count = MAX + 1;
+
+  while (--count >= 0)
+  {
+    printf("%d bottles of spring water on the shelf, "
+                 "%d bottles of spring water!\n", count, count);
+    printf("Take one bootle and let go in a circle,\n");
+    printf("%d bottles of spring water\n\n", count-1);
+  }
+
+  return EXIT_SUCCESS;
+}
