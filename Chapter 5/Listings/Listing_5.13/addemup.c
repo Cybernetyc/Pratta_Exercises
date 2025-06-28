2e6fe0933e2b5b1f14e388808f4a9c2114346b44
Cybernetyc <Necromounger1990@gmail.com>
2025-06-28 18:09:30 +0300

    Added code in Listing_5.13

--- Chapter 5/Listings/Listing_5.13/addemup.c
+++ Chapter 5/Listings/Listing_5.13/addemup.c
@@ -5,8 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main(void)  /* Finds the summ first 20 numbers */
 {
+  int count, sum;      ///< Announce operator
+  count = 0;           ///< Assingment operator
+  sum = 0;             ///< Assingment operator
 
-  return EXIT_SUCCESS;
+  while (count++ < 20) ///< While operator
+    sum = sum + count;
+
+  printf("sum = %d\n", sum); ///< Operator of calling function
+
+  return EXIT_SUCCESS;              ///< Return operator
 }
