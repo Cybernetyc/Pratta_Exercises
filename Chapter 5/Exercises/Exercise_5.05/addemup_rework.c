bc79bd8262203810a912963dada42709bcfe9701
Cybernetyc <Necromounger1990@gmail.com>
2025-07-02 21:51:00 +0300

    C: Added Exercise_5.05 (addemup_rework.c + CMakeLists.txt)
    - Introduced a new source file `addemup_rework.c` that calculates the sum of integers from user input down to 1 using a while loop.
    - Updated `CMakeLists.txt` to include `addemup_rework.c`, setting the C standard to 11, and creating an executable named `Exercise_5_05`.

---
+++ Chapter 5/Exercises/Exercise_5.05/addemup_rework.c
@@ -0,0 +1,22 @@
+/**
+ * addemup_rework.c -- This program is designed to show five types of operators
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int count = 0;      ///< loop counter
+  int sum   = 0;      ///< summary
+
+  printf("Enter a positive integer ( the value > 0 ):");
+  scanf("%d", &count);
+
+  while (count > 0)
+    sum = sum + count--;
+
+  printf("Summary = %d\n", sum);
+
+  return EXIT_SUCCESS;
+}
