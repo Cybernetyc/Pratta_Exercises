86de992c7e645c78504f9fbfb8602045228e91c5
Cybernetyc <Necromounger1990@gmail.com>
2025-07-06 21:02:26 +0300

    Added Listing_6.11 (sweetie2.c + CMakeLists.txt)
    - Introduced a new source file `sweetie2.c` demonstrating a `for` loop that prints a friendly message a specified number of times.
    - Added `CMakeLists.txt` to include `sweetie2.c`, set the C standard to 11, and create an executable named `Listing_6_11`.

---
+++ Chapter 6/Listings/Listing_6.11/sweetie2.c
@@ -0,0 +1,18 @@
+
+/**
+ * sweetie.c -- for cycle with counter
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  const int NUMBER = 22;
+  int count;
+
+  for (count = 1; count <= 100; count++ )
+    printf("Be my friend!\n");
+
+  return EXIT_SUCCESS;
+}
