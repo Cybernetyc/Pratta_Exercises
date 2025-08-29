a1684479943103f8e5e04d1bd6e90c874825d0a6
Cybernetyc <107352189+Cybernetyc@users.noreply.github.com>
2025-08-29 12:58:49 +0300

    Added Exercise_6.13 (double_octo.c + CMakeLists.txt)

---
+++ Chapter 6/Exercises/Exercise_6.13/double_octo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  constexpr int ARR_SIZE = 8;
+  int arr[ARR_SIZE] = {0}; ///< Null terminate array
+
+  int power = 1;
+  for (int i = 0; i < ARR_SIZE; ++i){
+    arr[i] = power;
+    power *= 2;
+  }
+
+  int print_counter = 0;
+  do
+  {
+    printf("%d\n", arr[print_counter]);
+    print_counter++;
+  }
+  while (print_counter < ARR_SIZE);
+  return EXIT_SUCCESS;
+}
